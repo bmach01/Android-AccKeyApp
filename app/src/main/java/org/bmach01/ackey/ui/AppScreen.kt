7918ee03340582f09456977bfb2304c3b19ba931
Bartosz Machniak <b.machniak01@gmail.com>
2024-10-10 16:12:40 +0200

    Minor change to AppScreen enum - title is not needed as it was opted for use of .name anyways

--- app/src/main/java/org/bmach01/ackey/ui/AppScreen.kt
+++ app/src/main/java/org/bmach01/ackey/ui/AppScreen.kt
@@ -1,8 +1,8 @@
 package org.bmach01.ackey.ui
 
-enum class AppScreen(val title: String) {
-    KeyScreen(title = "key_screen"),
-    PasswordKeyboardScreen(title = "password_keyboard_screen"),
-    RegisterScreen(title = "register_screen"),
-    SettingsScreen(title = "settings_screen")
+enum class AppScreen {
+    KeyScreen,
+    PasswordKeyboardScreen,
+    RegisterScreen,
+    SettingsScreen
 }
